1002ad81edcbfbac7eb6b3625f58f0602e46fb0d
Jashan <jashanxchopra@gmail.com>
2022-04-03 18:25:49 -0600

    fixed observation space, next up: probs

--- wordle_pomdp.jl
+++ wordle_pomdp.jl
@@ -1,4 +1,4 @@
-# This script defines Wordle as a POMDP for usage with the typical Julia libraries
+# This script defines Wordle as a MDP for usage with the typical Julia libraries
 
 using QuickPOMDPs: QuickPOMDP
 using POMDPModelTools: Uniform, Deterministic
@@ -6,62 +6,81 @@ using Wordle
 using Combinatorics
 include("./helper.jl")
 
+function words() 
+    # return the word list
+    return deepcopy(Wordle.VALID_WORD_LIST)
+end
+
 function wordle_states()
     # the total state space is the list of all valid words
-    # :return: Vector{String}, a list of valid Wordle actions
-    return deepcopy(Wordle.VALID_WORD_LIST)
+    # along with the range of game turns (0-7)
+    # 0 is the starting state, no guesses have been made 
+    # if we get to turn 7, we failed the game
+    # :return: Vector{Tuple(String, Int64)}, a list of valid Wordle actions
+    turn_range = collect(0:7)        
+    wordle_words = words()
+
+    # create all possible tuples 
+    states = Tuple{String, Int64}[]
+    for t in turn_range
+        for w in wordle_words
+            push!(states, (w, t))
+        end
+    end
+    return states
 
     # notes: 
-        # we might want to include the number of turns in the state space
-        # we could represent the state as a vector: 
-            # [String, Int64]
         # if we wanted both of them to be Int64 we could construct a mapping of Int64 to the Wordle.VALID_WORD_LIST
+        # not sure if this will be necesarry, storing the strings doesn't seem to be a big deal 
 end
 
 function wordle_actions()
-    # the total action space is the same as the state space, besides the empty word 
+    # the total action space is all the valid words, 
+    # we have to make a guess each turn  
     # :return: Vector{String}, a list of valid Wordle actions
-    return deepcopy(Wordle.VALID_WORD_LIST)
+    return words
 end
 
 function wordle_observations()
-    # # :output: observations: Array[Array[Symbol]]
-    # # the observation comes from the Wordle.jl "WordleResponse" class 
-
-    # # the wordle symbols correspond to the following:
-    #     # Wordle.CORRECT : "green" guess
-    #     # Wordle.PRESENT : "yellow" guess
-    #     # Wordle.INCORRECT : "black" guess
-    # # each state is an Array[Symbol] containing a symbol cooresponding to the guess type
-    # # example: [Wordle.CORRECT, Wordle.PRESENT, Wordle.INCORRECT, Wordle.CORRECT, Wordle.INCORRECT]
-
-    # # the total observation space is all possible 5-letter combinations of the set [Wordle.CORRECT, Wordle.PRESENT, Wordle.INCORRECT]
+    # :output: observations: Array[Array[Symbol]]
+    # the wordle symbols correspond to the following:
+        # Wordle.CORRECT : "green" guess
+        # Wordle.PRESENT : "yellow" guess
+        # Wordle.INCORRECT : "black" guess
+    # each state is an Array[Symbol] containing a symbol cooresponding to the guess type
+    # example: [Wordle.CORRECT, Wordle.PRESENT, Wordle.INCORRECT, Wordle.CORRECT, Wordle.INCORRECT]
+    # the total observation space is all possible 5-letter combinations of the set [Wordle.CORRECT, Wordle.PRESENT, Wordle.INCORRECT]
     
-    # # construct all possible tuples 
-    # # todo: how to do this efficiently, I'm so bad at Combinatorics 
-    # N = 5
-    # reverse.(Iterators.product(fill(0:N-1,N)...))[:]
+    # the possible symbols 
+    symbols = [:c, :p, :i]
 
-    # todo: this doesn't work
-    obs = Array[]
-    start_set = [:c, :p, :i, :na1, :na2]
-    unique_sets = collect(with_replacement_combinations(start_set, 5))
-    for set in unique_sets 
-        possibilities = collect(with_replacement_combinations(set, 5))
-        for o in possibilities
-            push!(obs, o)
+    # get all possible 5 letter combinations of those three symbols 
+    unique_sets = with_replacement_combinations(symbols, 5)
+
+    # then, get all permutations of each of those combinations
+    final_vec = []
+    for set in unique_sets
+        allperms = permutations(set, 5)
+        for perm in allperms
+            # push them to a single array 
+            push!(final_vec, perm)
         end
     end
-    final_obs = unique(collect(obs))
+    final_obs = unique(final_vec)
     return final_obs
 end
 
 function wordle_transition(s, a)
-    if s == a 
-        # if we guess the correct word, reset the state to a random one in the whole state space
-        return Uniform(wordle_states())
-    else
-        # if we haven't guessed the word, the state remains the same
+    # transition function for Wordle, given the state and action 
+    words_at_turn_zero = wordle_states[0:length(words())]
+    if s[1] == a 
+        # if we guess the correct word, reset the state 
+        return Uniform(words_at_turn_zero)
+    elseif s[2] == 7 
+        # if we are on the 7th turn, reset the state
+        return Uniform(words_at_turn_zero)
+    else 
+        # if we haven't guessed the word, and we have more turns left, the state remains the same
         return Deterministic(s)
     end
 end
@@ -89,9 +108,16 @@ end
 function wordle_reward(s, a)
     # :param: s: the current state
     # :param: a: the action, a Symbol object cooresponding to a 5 letter word from wordle_actions()
+    if s[1] == a 
+        # we found the word 
+        return 100.0
+    else
+        # the loss is equal to the turn we are on
+        return -1.0 * convert(Float64, s[2])
+    end
 
-    # todo: we may have to adjust these rewards... they are not set in stone 
-
+    # notes: 
+    # potentially old method of defining the reward function
     # a green guess (letter is in the right spot) is worth 10 points 
     # a yellow guess (correct letter, wrong spot) is worth 5 points 
     # a black guess (wrong letter) is worth -1 points
@@ -113,27 +139,17 @@ function wordle_reward(s, a)
     # global variable 
 
     # Sunberg says: 
-
     # but if you add rewards heuristically based on your human intuition, 
     # it is easy to create a (PO)MDP with an optimal policy does not achieve 
     # optimal performance with respect to the original objective. For example, 
     # if you added a heuristic reward for always guessing e's since they are common, 
     # the algorithm might over-guess e's even when its probability of winning is higher with another letter.
-
-    # in this case, maybe just a negative reward for an incorrect guess (maybe larger negatives depending on the guess number)
-    # and then a large reward if the guess is correct?
-    if s == a 
-        # we found the word 
-        # if we decide to define the state as a vector, make sure to get the word part of it 
-        return 100.0
-    else 
-        return -1.0
-    end
 end
 
 function wordle_init()
-    # the initial state is a random word
-    return Uniform(wordle_states())
+    # the initial state is a random word at turn 0
+    words_at_turn_zero = wordle_states[0:length(words())]
+    return Uniform(words_at_turn_zero)
 end
 
 function wordle(gamma=0.99)
@@ -154,5 +170,4 @@ function wordle(gamma=0.99)
     return m
 end
 
-# perform some testing on the POMDP? 
-# see POMDPTesting.jl
+wordle_observations()
