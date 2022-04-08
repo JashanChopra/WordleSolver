1ef2a06a46769e742d1df172423eaa36cf75c96b
Jashan <jashanxchopra@gmail.com>
2022-04-07 19:51:26 -0600

    brought in HW6 code to try to debug, I think our T/Z matrices need to be better defined

--- main.jl
+++ main.jl
@@ -2,76 +2,103 @@ using POMDPs
 using POMDPPolicies
 using POMDPSimulators
 using POMDPModelTools
+using POMDPTesting
 using Statistics: mean
 using QMDP
 using BeliefUpdaters
-
+using LinearAlgebra: dot
 
 # include functions that contain policies for evaluation 
 include("./wordle_pomdp.jl")
 include("./helper.jl")
+include("./value_iteration.jl")
 
-# using POMDPs
-# using DMUStudent.HW6
-# using POMDPModelTools: transition_matrices, reward_vectors, SparseCat, Deterministic, weighted_iterator, obs_weight
-# using QuickPOMDPs: QuickPOMDP
-# using POMDPModels: TigerPOMDP
-# using POMDPSimulators: RolloutSimulator
-# using BeliefUpdaters: DiscreteBelief, DiscreteUpdater
-# using SARSOP: SARSOPSolver
-# using QMDP: QMDPSolver
-# using POMDPTesting: has_consistent_distributions
-# using POMDPPolicies: FunctionPolicy
-# using LinearAlgebra: dot
+# Updater structure and function 
+struct HW6Updater{M<:POMDP} <: Updater
+    m::M
+end
 
+function Z(m::POMDP, a, sp, o)
+    # return a probability from a POMDP's observation table for a given action, future state, and observation
+    return pdf(observation(m, a, sp), o)
+end
 
-# # Updater structure and function 
-# struct HW6Updater{M<:POMDP} <: Updater
-#     m::M
-# end
+function T(m::POMDP, s, a, sp)
+    # return a probability from a POMDP's transition table for a given state, action, and future state
+    return pdf(transition(m, s, a), sp)
+end
 
-# function Z(m::POMDP, a, sp, o)
-#     # return a probability from a POMDP's observation table for a given action, future state, and observation
-#     return pdf(observation(m, a, sp), o)
-# end
+function POMDPs.update(updater::HW6Updater, b::DiscreteBelief, a, o)
+    # perform a belief update with a discrete Bayesian filter
+    pomdp = updater.m
+    S = ordered_states(pomdp)
 
-# function T(m::POMDP, s, a, sp)
-#     # return a probability from a POMDP's transition table for a given state, action, and future state
-#     return pdf(transition(m, s, a), sp)
-# end
+    # the new belief
+    b_prime = zeros(length(S))
 
-# function POMDPs.update(updater::HW6Updater, b::DiscreteBelief, a, o)
+    # update the belief vector for each state in the POMDP 
+    for (sp_idx, sp) in enumerate(S)
+        po = Z(pomdp, a, sp, o)
+        pt = 0.0 
+        for (s_idx, s) in enumerate(S)
+            pt += T(pomdp, s, a, sp) * b.b[s_idx]
+        end
+        b_prime[sp_idx] = po * pt
+    end
+
+    # normalize the belief
+    b_prime ./= sum(b_prime) 
+
+    return DiscreteBelief(updater.m, b_prime, check=true)
+end
+
+function POMDPs.initialize_belief(updater::HW6Updater, distribution::Any)
+    # initialize the belief as a vector of zeros, with length equal to the number of states in the POMDP
+    states = ordered_states(updater.m)
+    belief = zeros(length(states))
+
+    # for each state, initialize a belief based on the given distribution 
+    for s in ordered_states(updater.m)
+        belief[stateindex(updater.m, s)] = pdf(distribution, s)
+    end
+    return DiscreteBelief(updater.m, belief)
+end
+
+# Policy & action function 
+struct HW6AlphaVectorPolicy{A} <: Policy
+    alphas::Vector{Vector{Float64}}
+    alpha_actions::Vector{A}
+end
+
+function POMDPs.action(p::HW6AlphaVectorPolicy, b::DiscreteBelief)
+    # given a belief b, find the alpha vector that gives the higehst value at that belief point
+    idx = argmax([dot(a, b.b) for a in p.alphas])
+    return p.alpha_actions[idx]
+end
+
+# QMDP Solver function 
+function qmdp_solve(m, discount=discount(m))
+
+    # turn the POMDP into an MDP and perform value iteration 
+    mdp = UnderlyingMDP(m)
     
-#     # perform a belief update with a discrete Bayesian filter
-#     pomdp = updater.m
-#     S = states(pomdp)
+    # load in the transition matrices and rewards
+    T = transition_matrices(mdp)
+    R = reward_vectors(mdp)
+    tol = 1e-35         # convergence tolerance
+    loops = 1e7         # maximum number of 
+    
+    # perform value iteration 
+    _, _, Qmatrix = value_iteration_vectorized(m, T, R, tol, discount, loops)
 
-#     # the new belief
-#     belief = zeros(length(S))
-
-#     # update the belief vector for each state in the POMDP 
-#     for s in S
-#         prob_observation = Z(pomdp, a, s, o)
-#         prob_transition_sum = sum((T(pomdp, s, a, sp) * b.b[stateindex(pomdp, sp)]) for sp in S)
-#         belief[stateindex(pomdp, s)] = prob_observation * prob_transition_sum
-#     end
-
-#     # normalize the belief
-#     belief ./= sum(belief) 
-
-#     return DiscreteBelief(updater.m, belief, check=true)
-# end
-
-# function POMDPs.initialize_belief(updater::HW6Updater, distribution::Any)
-#     # initialize the belief as a vector of zeros, with length equal to the number of states in the POMDP
-#     belief = zeros(length(states(updater.m)))
-
-#     # for each state, initialize a belief based on the given distribution 
-#     for s in states(updater.m)
-#         belief[stateindex(updater.m, s)] = pdf(distribution, s)
-#     end
-#     return DiscreteBelief(updater.m, belief)
-# end
+    # the psuedoalpha vectors are the Qmatrix entries
+    acts = ordered_actions(m)
+    alphas = Vector{Float64}[]
+    for (i, _) in enumerate(acts)
+        push!(alphas, Qmatrix[i, :])
+    end
+    return HW6AlphaVectorPolicy(alphas, acts)
+end
 
 function main()
     # define the wordle POMDP 
@@ -100,10 +127,9 @@ function main()
     # println("The average reward over ", n, " games for the random policy was: ", reward)
     # println("Out of ", n, " games, ", correct, " were correctly guessed")
 
-    # Evaluate with the QMDP Solver
-    solver = QMDPSolver()
-    # up = HW6Updater(m)
-    up = DiscreteUpdater(m)
+    # Evaluate with the HW6 QMDP Solver for checking     
+    solver = qmdp_solve(m)
+    up = HW6Updater(m)
     policy = solve(solver, m)
     @show mean(simulate(RolloutSimulator(), m, policy, up) for _ in 1:1000)
     rsum = 0.0
